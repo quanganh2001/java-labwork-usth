3fc32b381b992846a16cb461f76717599c41b6c5
quanganh2001 <cocoi5659@gmail.com>
2021-09-09 14:50:56 +0700

    Print true if the given day and month fall between March 20 (inclusive) and June 20 (inclusive)

---
+++ BuiltInTypes/SpringSeason.java
@@ -0,0 +1,13 @@
+package BuiltInTypes;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        int month = 3;
+        int day = 20;
+        boolean isSpring = (month == 3 && day >= 20 && day <= 31)
+                        || (month == 4 && day >=  1 && day <= 30)
+                        || (month == 5 && day >=  1 && day <= 31)
+                        || (month == 6 && day >=  1 && day <= 20);
+        System.out.println(isSpring);
+    }
+}
