978741d012ef5e7d21124dbfc0c7ee6f8567762f
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2021-09-09 14:48:07 +0700

    Update comment
    Write a program takes a double command-line argument t (in degrees) and prints the value of sin(2t) + sin(3t).

--- BuiltInTypes/SumOfSines.java
+++ BuiltInTypes/SumOfSines.java
@@ -2,6 +2,7 @@ package BuiltInTypes;
 
 public class SumOfSines {
     public static void main(String[] args) {
+        // Write a program takes a double command-line argument t (in degrees) and prints the value of sin(2t) + sin(3t).
         double degrees = 30;
         double radians = 30;
         double sum = Math.sin(2 * radians) + Math.sin(3 * radians);
