7fdebcc572768039670647c279b6f60f78f9c941
quanganh2001 <cocoi5659@gmail.com>
2021-09-09 14:30:46 +0700

    prints your initials using nine rows of asterisks like the one below

---
+++ FirstProgram/Initials.java
@@ -0,0 +1,15 @@
+package FirstProgram;
+public class Initials {
+    public static void main(String[] args) {
+        // prints your initials using nine rows of asterisks like the one below
+        System.out.println("**        ***    **********      **             *             **");
+        System.out.println("**      ***      **        **     **           ***           **");
+        System.out.println("**    ***        **         **     **         ** **         **");
+        System.out.println("**  ***          **          **     **       **   **       **");
+        System.out.println("*****            **          **      **     **     **     **");
+        System.out.println("**  ***          **          **       **   **       **   **");
+        System.out.println("**    ***        **         **         ** **         ** **");
+        System.out.println("**      ***      **        **           ***           ***");
+        System.out.println("**        ***    **********              *             *");
+    }
+}
