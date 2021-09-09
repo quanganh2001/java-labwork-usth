0cc27d9f1497f5a74586e56bb03021579fec291b
quanganh2001 <cocoi5659@gmail.com>
2021-09-09 14:34:56 +0700

    Modify UseArgument.java to make a program UseThree.java that takes three names and prints out a proper sentence with the names in the reverse of the order given

---
+++ FirstProgram/UseThree.java
@@ -0,0 +1,10 @@
+package FirstProgram;
+public class UseThree {
+    public static void main(String[] args) {
+        // Modify UseArgument.java to make a program UseThree.java that takes three names and prints out a proper sentence with the names in the reverse of the order given
+        System.out.print("Hi, Alice");
+        System.out.print(", Bob");
+        System.out.print(", and Carol");
+        System.out.println(".");
+    }
+}
