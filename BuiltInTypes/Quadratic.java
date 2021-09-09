e40a0daa89ad6aa472c8b08948698d4405f9801e
quanganh2001 <cocoi5659@gmail.com>
2021-09-09 14:49:33 +0700

    Given b and c, solves for the roots of x*x + b*x + c

---
+++ BuiltInTypes/Quadratic.java
@@ -0,0 +1,17 @@
+package BuiltInTypes;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        double b = 0;
+        double c = -1;
+
+        double discriminant = b * b - 4.0 * c;
+        double sqroot = Math.sqrt(discriminant);
+
+        double root1 = (-b + sqroot) / 2.0;
+        double root2 = (-b + sqroot) / 2.0;
+
+        System.out.println(root1);
+        System.out.println(root2);
+    }
+}
