111a46189d69676c141d111682165b1b5a2b0083
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2021-09-11 08:55:21 +0700

    Add comment and edit to user input

--- FirstProgram/UseThree.java
+++ FirstProgram/UseThree.java
@@ -1,10 +1,15 @@
 package FirstProgram;
 public class UseThree {
+
     public static void main(String[] args) {
-        // Modify UseArgument.java to make a program UseThree.java that takes three names and prints out a proper sentence with the names in the reverse of the order given
-        System.out.print("Hi, Alice");
-        System.out.print(", Bob");
-        System.out.print(", and Carol");
+        // takes three names and prints out a proper sentence with the names in the reverse of the order given
+        System.out.print("Hi, ");
+        System.out.print(args[2]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(", and ");
+        System.out.print(args[0]);
         System.out.println(".");
     }
+
 }
