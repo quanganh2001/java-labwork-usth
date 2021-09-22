b1eaf42ffcadc7b05eeb9c081ca37f6b7d6d8f62
quanganh2001 <cocoi5659@gmail.com>
2021-09-22 10:38:27 +0700

    a Java tester class “CowTestDrive” to implement the OOP class design: “Cow”

---
+++ Labwork2/CowTestDrive.java
@@ -0,0 +1,13 @@
+// a Java tester class “CowTestDrive” to implement the OOP class design: “Cow”
+package Labwork2;
+
+import java.util.*;
+import java.lang.Math;
+
+public class CowTestDrive {
+    public static void main(String[] args) {
+        Cow cow = new Cow();
+        cow.setAge(4);
+        cow.moo();
+    }
+}
