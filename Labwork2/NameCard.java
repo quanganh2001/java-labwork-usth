4036e3807ee4057881cfda22abb2607db22f89d6
quanganh2001 <cocoi5659@gmail.com>
2021-09-22 10:39:11 +0700

    / Write a Java class “NameCard” and a Java tester class “NameCardTestDrive”to present the corresponding NameCard design:

---
+++ Labwork2/NameCard.java
@@ -0,0 +1,23 @@
+// Write a Java class “NameCard” and a Java tester class “NameCardTestDrive”to present the corresponding NameCard design:
+// Apply the “Encapsulation” concept in your classes
+package Labwork2;
+
+import java.util.*;
+import java.lang.Math;
+
+public class NameCard {
+    public String name;
+    public String phone;
+    public String email;
+    public void getName() {
+        System.out.println(name);
+    }
+
+    public void getPhone() {
+        System.out.println(phone);
+    }
+
+    public void getEmail() {
+        System.out.println(email);
+    }
+}
